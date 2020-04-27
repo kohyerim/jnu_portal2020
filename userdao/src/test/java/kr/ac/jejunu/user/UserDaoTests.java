1d7dcf89065fea44fce5aebb9f79d39cf06d45c7
kohyerim <kohyerim78@gmail.com>
2020-04-27 22:42:34 +0900

    0424 4번째 영상 실습

--- userdao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -1,6 +1,7 @@
 package kr.ac.jejunu.user;
 
 import javafx.application.Application;
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -49,32 +50,36 @@ public class UserDaoTests {
         assertThat(insertedUser.getPassword(), is(password));
     }
 
-//
-//    @Test
-//    public void getHalla() throws SQLException, ClassNotFoundException {
-//        Integer id = 1;
-//
-//        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-//        UserDao userDao = new UserDao(connectionMaker);
-//        User user = userDao.get(id);
-//        assertThat(user.getId(), is(id));
-//        assertThat(user.getName(), is(name));
-//        assertThat(user.getPassword(), is(password));
-//    }
-//
-//
-//    @Test
-//    public void insertHalla() throws SQLException, ClassNotFoundException {
-//        User user = new User();
-//        user.setName(name);
-//        user.setPassword(password);
-//        ConnectionMaker connectionMaker = new HallaConnectionMaker();
-//        UserDao userDao = new UserDao(connectionMaker);
-//        userDao.insert(user);
-//        assertThat(user.getId(), greaterThan(0));
-//
-//        User insertedUser = userDao.get(user.getId());
-//        assertThat(insertedUser.getName(), is(name));
-//        assertThat(insertedUser.getPassword(), is(password));
-//    }
+    @Test
+    public void update() throws SQLException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        String updatedName = "hyerim";
+        String updatedPassword = "1111";
+        user.setName(updatedName);
+        user.setPassword(updatedPassword);
+
+        userDao.update(user);
+        User updatedUser = userDao.get(user.getId());
+        assertThat(updatedUser.getName(), is(updatedName));
+        assertThat(updatedUser.getPassword(), is(updatedPassword));
+    }
+
+    @Test
+    public void delete() throws SQLException {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        userDao.delete(user.getId());
+
+        User deletedUser = userDao.get(user.getId());
+
+        assertThat(deletedUser, IsNull.nullValue());
+
+    }
 }
