33bfa95fe254927975beda19c747524398fb2547
kohyerim <kohyerim78@gmail.com>
2020-05-01 16:45:12 +0900

    0501 2번째 영상

--- userdao/src/main/java/kr/ac/jejunu/user/UpdateStatementStrategy.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UpdateStatementStrategy.java
@@ -1,14 +1,17 @@
 package kr.ac.jejunu.user;
 
-import javax.imageio.plugins.jpeg.JPEGImageReadParam;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UpdateStatementStrategy implements StatementStrategy {
+    private User user;
+    public UpdateStatementStrategy(User user) {
+        this.user = user;
+    }
+
     @Override
-    public PreparedStatement makeStatement(Object object, Connection connection) throws SQLException {
-            User user = (User) object;
+    public PreparedStatement makeStatement(Connection connection) throws SQLException {
             PreparedStatement preparedStatement = connection.prepareStatement("update userinfo set name = ?, password = ? where id = ?");
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getPassword());
